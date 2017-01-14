1e843727fe53dbeabaa4d3ee0f5596ab41b9396f
Tommaso Marcelli <tommaso.marcelli@sicomunicaweb.it>
2017-01-14 17:55:44 +0100

    Include Foundation SASS in Vue components, with mixins and variables

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+  },
+};
+</script>
+
+
+
+<style lang="scss">  
+  @import "./styles/app"; // Import app.scss in the main app
+
+  #app {
+    @include grid-row();
+  }
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <a id="button" href="#">Click Me</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+  @import "../styles/app"; // Import app.scss in the component (duplication is removed during the build)
+
+  #button {
+    @include button();
+  }
+</style>
