e5852aad93e38fdd0eb9201548d0f277f445b9af
Grant Carthew <grant@carthew.net>
2017-09-26 21:32:22 +1000

    Replace foundation import with plugin import

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import jQuery from 'jquery';
 import Vue from 'vue';
+import './foundation';
 import App from './App';
 import router from './router';
-
-window.jQuery = jQuery;
-window.$ = jQuery;
-
-require('motion-ui');
-require('what-input');
-require('foundation-sites');
 
 /* eslint-disable no-new */
 new Vue({
