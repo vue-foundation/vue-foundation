7c30d6a1745dcf7e70e7f0ed4a26cadb981f2b26
Grant Carthew <grant@carthew.net>
2017-10-04 09:22:22 +1000

    Added foundation-sites into babel transpile

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -47,7 +47,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/foundation-sites/js')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
